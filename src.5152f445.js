550952bd5fb101f8d7f22657f937ec475d07546f
Stanislav-Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-01-18 17:54:02 +0000

    Deploying to gh-pages from @ Stanislav-Sobolev/ice-cream-landing@bde207cf039a022e02d5eeac425d8a521117a89c 🚀

--- src.5152f445.js
+++ src.5152f445.js
@@ -13,4 +13,4 @@ const e=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clicka
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/slider.js"),require("./js/menu.js"),require("./js/arrow.js"),require("./js/buynow.js"),require("./js/franchise.js");
 },{"./sass/main.scss":"clu1","./js/slider.js":"wzuc","./js/menu.js":"i0CD","./js/arrow.js":"Dcx1","./js/buynow.js":"n7ip","./js/franchise.js":"wkSI"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-landing/src.be8c21cf.js.map
+//# sourceMappingURL=/ice-cream-landing/src.5152f445.js.map
