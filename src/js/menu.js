a8484608396013b7aed76e22223115297ed120a5
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-01-18 14:59:50 +0200

    fix js

--- src/js/menu.js
+++ src/js/menu.js
@@ -25,9 +25,6 @@
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
