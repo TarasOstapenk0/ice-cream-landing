fd7c2ba27d9d2021e0dc943346d3d4f4b061978d
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-01-18 19:50:28 +0200

    add counter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './sass/main.scss';
 
-// import './js/modal-about.js';
 import './js/slider.js';
 import './js/menu.js';
 import './js/arrow.js';
 import './js/buynow.js';
-import './js/franchise';
+import './js/franchise.js';
