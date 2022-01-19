acd2d34029e9d135396a33c0f352ab0855055474
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-01-19 20:31:50 +0200

    counter first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './sass/main.scss';
 
+import './js/counter.js';
 import './js/slider.js';
 import './js/menu.js';
 import './js/arrow.js';
 import './js/buynow.js';
 import './js/buynow-mob.js';
 import './js/franchise.js';
-import './js/counter.js';
 
 // LOCATIONS
 import './js/locations.js';
